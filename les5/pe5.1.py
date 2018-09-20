bcf535e32d8ca6bd54d2274a92b59a32b8057350
Famebringer <muridi.aboukar@student.hu.nl>
2018-09-20 15:41:17 +0200

    P5 tot opdracht 2 saved versions

---
+++ les5/pe5.1.py
@@ -0,0 +1,7 @@
+# klein programma
+score = eval(input (' Wat voor score heb je behaald?: '))
+if score >= 6:
+    print (" Je hebt jou examen behaald!")
+
+else:
+    print (" Helaas je hebt jou examen niet behaald, succes met de herkansing!")
---
+++ les5/pe5.2.py
@@ -0,0 +1,7 @@
+age = eval(input (' Hoe oud ben je?: '))
+pp =  str (input ( ' Heb je een Nederlandse Paspoort?'))
+if age >= 18 and pp =='Ja':
+    print(" Jij mag stemmen!")
+
+else:
+    print(" Helaas jij mag nog niet stemmen!")
