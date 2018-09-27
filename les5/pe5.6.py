33ad14895ee49a51d9806aaad18b89bb20c5ebb5
Famebringer <muridi.aboukar@student.hu.nl>
2018-09-27 15:25:20 +0200

    Klaar met Chapter 5 compleet nu de rest van CHapter 6

--- les5/pe5.6.py
+++ les5/pe5.6.py
@@ -1,6 +1,14 @@
-s = "Guido van Rossum heeft programmeertaal Python bedacht."
-if 'a, ,e ,o ,u ,i' in s:
-    print(s)
+#   Schrijf een for-loop die langs alle letters van een string loopt en de letter uitprint,
+#  maar alleen als het een klinker is ('aeiou').
+# Gebruik de volgende string:
 
-else:
-    print(" Er is niks!")
+s = "Guido van Rossum heeft programmeertaal Python bedacht."
+klinkers = []
+
+for klinker in s:
+    if klinker [0]=='a,e,o,u,i':
+        s.append(klinker)
+
+print(s)
+
+
