ad3deea4a78285ca959d134d8f7d1a95af13383a
Famebringer <muridi.aboukar@student.hu.nl>
2018-09-30 13:51:38 +0200

    Einde p6, nog final assignment fixen

---
+++ les6/pe6.6_NS-Final_Assignment.py
@@ -0,0 +1,30 @@
+def standaardprijs(afstandKM):
+    res = 0
+    vastbedrag = 15
+    for i in afstandKM:
+        if i <50:
+            res = res + i*0.80
+
+        else:
+            print (15 + 0.60 * 15)
+    return res
+
+# print(standaardprijs([50]))
+
+def ritprijs(leeftijd, weekendrit, afstandKM):
+    res = 35
+    jo_oud = 30
+    for a in weekendrit:
+        if a == " Ja ":
+            res = res / standaardprijs()
+            return res
+
+
+
+     for a in leeftijd
+          elif b <=12 and  >65:
+            jo_oud = jo_oud / standaardprijs()
+            return res
+
+
+
